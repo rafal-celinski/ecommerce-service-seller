1a3159b995147be7ba5819e15cd54c1c169c78af
Przemysław Walecki <p.walecki12@gmail.com>
2024-05-14 16:42:28 +0200

    Revert "[PIS-8] Formatted result json"
    This reverts commit 764bd8f7a596a3e5cb5b77b135fc5af4de4b0140.

--- api/src/main/java/pis24l/projekt/api/controllers/ProductSearchController.java
+++ api/src/main/java/pis24l/projekt/api/controllers/ProductSearchController.java
@@ -1,33 +1,28 @@
 package pis24l.projekt.api.controllers;
 
 import org.apache.tomcat.jni.Local;
-import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import pis24l.projekt.api.dtos.ProductDTO;
 import pis24l.projekt.api.model.Product;
 import pis24l.projekt.api.repositories.ProductRepository;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/products")
 public class ProductSearchController {
     private final ProductRepository productRepository;
-    private  final ModelMapper modelMapper;
 
     public ProductSearchController(ProductRepository productRepository) {
         this.productRepository = productRepository;
-        this.modelMapper = new ModelMapper();
     }
 
     @GetMapping("/search")
-    public List<ProductDTO> searchProducts(
+    public List<Product> searchProducts(
             @RequestParam(required = false, defaultValue = "") String search,
             @RequestParam(required = false) Long category,
             @RequestParam(required = false) Long subcategory,
@@ -37,16 +32,11 @@ public class ProductSearchController {
             @RequestParam(required = false,defaultValue = "false") Boolean isTesting) {
         // Assuming search is for the product title
         if (!isTesting){
-            return mapProductsToDTOs(productRepository.findAll());
+            return productRepository.findAll();
         }
         else {
-            return mapProductsToDTOs(productRepository.findByPriceBetweenAndTitleContainingAndCategoryAndSubcategoryAndLocation(
-                    minPrice, maxPrice, search, category, subcategory, location));
+            return productRepository.findByPriceBetweenAndTitleContainingAndCategoryAndSubcategoryAndLocation(
+                    minPrice, maxPrice, search, category, subcategory, location);
         }
-    }
-    private List<ProductDTO> mapProductsToDTOs(List<Product> products) {
-        return products.stream()
-                .map(product -> modelMapper.map(product, ProductDTO.class))
-                .collect(Collectors.toList());
     }
 }
