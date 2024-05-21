04b72de24ca2a206b7ba47e54af2627fadc8630e
Przemysław Walecki <p.walecki12@gmail.com>
2024-05-21 23:20:35 +0200

    PIS-8 New controller endpoint

--- api/src/main/java/pis24l/projekt/api/controllers/ProductSearchController.java
+++ api/src/main/java/pis24l/projekt/api/controllers/ProductSearchController.java
@@ -1,38 +1,42 @@
 package pis24l.projekt.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pis24l.projekt.api.model.Product;
 import pis24l.projekt.api.service.ProductSearchService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductSearchController {
 
-    private final ProductSearchService productService;
+    private final ProductSearchService productSearchService;
 
     @Autowired
-    public ProductSearchController(ProductSearchService productService) {
-        this.productService = productService;
+    public ProductSearchController(ProductSearchService productSearchService) {
+        this.productSearchService = productSearchService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = productSearchService.getProductById(id);
+        return ResponseEntity.ok(product);
     }
 
     @GetMapping("/search")
-    public Page<Product> searchProducts(
-            @RequestParam(required = false, defaultValue = "") String search,
+    public ResponseEntity<Page<Product>> searchProducts(
+            @RequestParam(required = false) String search,
             @RequestParam(required = false) Long category,
             @RequestParam(required = false) Long subcategory,
             @RequestParam(required = false) BigDecimal minPrice,
             @RequestParam(required = false) BigDecimal maxPrice,
             @RequestParam(required = false) String location,
-            @RequestParam(required = false, defaultValue = "0") int page,
-            @RequestParam(required = false, defaultValue = "10") int size) {
-        Pageable pageable = PageRequest.of(page, size);
-        return productService.searchProducts(search, category, subcategory, minPrice, maxPrice, location, pageable);
+            Pageable pageable) {
+        Page<Product> products = productSearchService.searchProducts(search, category, subcategory, minPrice, maxPrice, location, pageable);
+        return ResponseEntity.ok(products);
     }
 }
