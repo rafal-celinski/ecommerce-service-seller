6fb434f733428a95e26dee8f5004b83f6db9d475
Rafał Celiński <dev@rafal.celinski.xyz>
2024-06-13 00:46:54 +0200

    PIS-21 Created Orders page

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Auction from './Auction';
 import AddAuction from './AddAuction';
+import Orders from './Orders';
 
 function App() {
     return (
@@ -12,7 +13,7 @@ function App() {
                     <Route path="/" element=<Home /> />
                     <Route path="/auction/:auctionId" element=<Auction /> />
                     <Route path="/add" element=<AddAuction /> />
-
+                    <Route path="/orders" element=<Orders /> />
                 </Routes>
             </BrowserRouter>
     );
