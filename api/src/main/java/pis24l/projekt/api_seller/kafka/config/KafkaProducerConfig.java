522720b08dbb27b7edefb0898700cb0c40de05d3
Mateusz Lukasiewicz <mateuszlu2106@gmail.com>
2024-06-12 23:39:19 +0200

    Update KafkaProducerConfig.java

--- api/src/main/java/pis24l/projekt/api_seller/kafka/config/KafkaProducerConfig.java
+++ api/src/main/java/pis24l/projekt/api_seller/kafka/config/KafkaProducerConfig.java
@@ -26,6 +26,21 @@ public class KafkaProducerConfig {
     }
 
     @Bean
+    public ProducerFactory<String, String> producerSendFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaSendTemplate() {
+        return new KafkaTemplate<>(producerSendFactory());
+    }
+
+
+    @Bean
     public KafkaTemplate<String, OrderResponse> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
