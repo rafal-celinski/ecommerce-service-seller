77b3a421632b5820050ec243d51ef8f5bd96fc76
Przemysław Walecki <p.walecki12@gmail.com>
2024-06-13 21:49:42 +0200

    Merge Fix

--- api/src/main/java/pis24l/projekt/api_seller/models/Image.java
+++ api/src/main/java/pis24l/projekt/api_seller/models/Image.java
@@ -1,39 +1,39 @@
-//package pis24l.projekt.api_seller.models;
-//
-//import org.springframework.data.mongodb.core.mapping.Document;
-//
-//import javax.persistence.*;
-//
-//@Document(collection = "image")
-//@Entity
-//public class Image {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private String id;
-//
-//    @Column(name = "product_id")
-//    private String productId;
-//
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public void setId(String id) {
-//        this.id = id;
-//    }
-//
-//    public String getProductId() {
-//        return productId;
-//    }
-//
-//    public void setProductId(String productId) {
-//        this.productId = productId;
-//    }
-//
-//    public Image(String productId) {
-//        this.productId = productId;
-//    }
-//    protected Image() { }
-//}
-//
+package pis24l.projekt.api_seller.models;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.*;
+
+@Document(collection = "image")
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(name = "product_id")
+    private String productId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Image(String productId) {
+        this.productId = productId;
+    }
+    protected Image() { }
+}
+
